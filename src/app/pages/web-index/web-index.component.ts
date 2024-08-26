5c1fc5e9ed8d2a552c475416f8352a9b23d809ff
Azrielx86 <edgar1569@outlook.com>
2024-08-25 20:40:00 -0600

    Removed extra items from ArticleModel.ts

--- src/app/pages/web-index/web-index.component.ts
+++ src/app/pages/web-index/web-index.component.ts
@@ -17,26 +17,12 @@ export class WebIndexComponent {
   articles: ArticleModel[] = [
     {
       title: "Non-Terrestrial Networks (NTN)",
-      characteristics: [
-        "todo 1",
-        "todo 2"
-      ],
-      about: "",
-      objectives: "Something more...",
       headerImage: "https://static.wixstatic.com/media/7bd1be_9586caef120044e4a229adfdfc4612e6~mv2.jpg",
-      imageSource: "https://www.satelliteevolution.com/post/gatehouse-satcom-contributes-to-the-first-3gpp-5g-nb-iot-ntn-release",
       markdownSource: "articles/ntn_networks.md"
     },
     {
       title: "6G Networks",
-      characteristics: [
-        "todo 1",
-        "todo 2"
-      ],
-      about: "",
-      objectives: "Something more...",
       headerImage: "https://assets.techrepublic.com/uploads/2023/01/tr1323-6g-what-is-it.jpeg",
-      imageSource: "No source",
       markdownSource: "articles/6g_networks.md"
     }
   ];
